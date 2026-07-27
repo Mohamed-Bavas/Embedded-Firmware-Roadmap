fd54100997a9d08c80ed640433f3e0ff3f3ce79f
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-07-27 15:50:12 +0530

    added

---
+++ Day01_C_Basics/add_two_numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a\n");
+    scanf("%d",&a);
+    printf("Enter b\n");
+    scanf("%d",&b);
+    printf("Sum of two numbers =%d",a+b);
+}
