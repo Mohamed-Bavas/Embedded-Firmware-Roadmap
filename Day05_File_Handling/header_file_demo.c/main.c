a4cb40dd12211ae3786087f72d239683f1ea7502
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-07-31 21:14:42 +0530

    added

---
+++ Day05_File_Handling/header_file_demo.c/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myheader.h"
+int main()
+{
+    printf("Addition = %d\n", add(10, 20));
+    printf("PI = %.2f\n", PI);
+    return 0;
+}
---
+++ Day05_File_Handling/header_file_demo.c/myheader.h
@@ -0,0 +1,5 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+#define PI 3.14
+int add(int, int);
+#endif
