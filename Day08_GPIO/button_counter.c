6df941590a5801d0e1209a3c8707f5d427c96176
Mohamed-Bavas <mohamedbavasmohamedbavas@gmail.com>
2026-08-03 20:24:03 +0530

    added

---
+++ Day08_GPIO/button_counter.c
@@ -0,0 +1,34 @@
+#include <reg51.h>
+
+sbit SW = P1^0;
+
+void delay()
+{
+    unsigned int i,j;
+    for(i=0;i<10;i++)
+        for(j=0;j<100;j++);
+}
+
+void main()
+{
+    unsigned char count = 0;
+
+    P2 = 0x00;
+
+    while(1)
+    {
+        if(SW == 0)
+        {
+           delay();
+
+            if(SW == 0)
+            {
+                count++;
+                P2 = count;
+
+                while(SW == 0);   // Wait for release
+                delay();
+            }
+        }
+    }
+}
